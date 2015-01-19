193ae1bb38af22e176a82dc9e1daecea9b78a3a3
Vjekoslav Nesek <vnesek@instantcom.net>
2015-01-19 09:20:13 +0100

    Added fileIntoAlias setting for DefaultDeliveryAgent

--- src/main/java/com/nmote/mcf/DefaultDeliveryAgent.java
+++ src/main/java/com/nmote/mcf/DefaultDeliveryAgent.java
@@ -1,15 +1,50 @@
 package com.nmote.mcf;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.inject.Inject;
 
 public class DefaultDeliveryAgent implements DeliveryAgent {
 
 	@Override
 	public void deliver(QueueMessage msg, Delivery delivery) throws IOException {
-		DeliveryAgent agent;
 		String dest = delivery.getDestination();
+		if (aliases != null) {
+			String r = aliases.get(dest);
+			if (r != null) {
+				dest = r;
+			}
+		}
+		final DeliveryAgent agent = deliveryAgentFor(dest);
+		if (agent == null) {
+			throw new IOException("unsupported delivery protocol: " + delivery.getDestination());
+		}
+		agent.deliver(msg, delivery);
+	}
+
+	@Inject
+	public void setFileIntoAlias(@Named("fileIntoAlias") String alias) {
+		Map<String, String> aliases = new HashMap<>();
+		for (String a : StringUtils.split(alias)) {
+			String[] b = StringUtils.split(a, "=>");
+			if (b.length != 2) {
+				throw new IllegalArgumentException("invalid fileIntoAlias: " + a);
+			}
+			aliases.put(b[0], b[1]);
+		}
+		if (!aliases.isEmpty()) {
+			this.aliases = aliases;
+		}
+	}
+
+	protected DeliveryAgent deliveryAgentFor(String dest) {
+		DeliveryAgent agent;
 		if (dest.startsWith("smtp:")) {
 			agent = smtp;
 		} else if (dest.startsWith("mbox:")) {
@@ -19,20 +54,22 @@ public class DefaultDeliveryAgent implements DeliveryAgent {
 		} else if (dest.startsWith("discard")) {
 			agent = discard;
 		} else {
-			throw new IOException("unknown delivery protocol: " + dest);
+			agent = null;
 		}
-		agent.deliver(msg, delivery);
+		return agent;
 	}
 
-	@Inject
-	private SmtpDeliveryAgent smtp;
-	
-	@Inject
-	private MBoxDeliveryAgent mbox;
-	
-	@Inject
-	private MaildirDeliveryAgent maildir;
-	
-	@Inject
-	private DiscardDeliveryAgent discard;
+	@Inject(optional = true)
+	protected DiscardDeliveryAgent discard;
+
+	@Inject(optional = true)
+	protected MaildirDeliveryAgent maildir;
+
+	@Inject(optional = true)
+	protected MBoxDeliveryAgent mbox;
+
+	@Inject(optional = true)
+	protected SmtpDeliveryAgent smtp;
+
+	private Map<String, String> aliases;
 }
